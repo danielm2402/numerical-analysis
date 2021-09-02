ded52b95b3ddb952aef79ccd0257017f52df3caa
Daniel Munoz <dani.m16@hotmail.com>
2021-09-01 20:08:16 -0500

    added simpson 3/8

--- simpsons_rule/main.cpp
+++ simpsons_rule/main.cpp
@@ -8,6 +8,9 @@ int main() {
         simpson s([](double x)->double{
             return exp(x*x);
         },0.0f, 1.0f);
-        cout<<"Integral por metodo de Simpson 1/3"<<endl;
+        cout<<"Integral por metodo de Simpson 1/3: "<<s.un_tercio(8)<<endl; //N DEBE SER PAR
+        cout<<"Integral por metodo de Simpson 3/8: "<<s.tres_octavos(9)<<endl; //N DEBE SER IMPAR
+
+
     return 0;
 }
--- simpsons_rule/simpson.h
+++ simpsons_rule/simpson.h
@@ -5,14 +5,16 @@
 #ifndef SIMPSONS_RULE_SIMPSON_H
 #define SIMPSONS_RULE_SIMPSON_H
 #include <functional>
+#include <utility>
 
 using std::function;
+using std::cout;
+using std::endl;
 
 class simpson{
 public:
-    simpson(function<double(double)> p_f, double p_a, double p_b){
-        a = p_a;
-    }
+    simpson(function<double(double)> p_f, double p_a, double p_b):f(p_f), a(p_a), b(p_b){};
+
     double un_tercio(int n){
         double resultado {0.0f};
         double x = a ;
@@ -21,7 +23,7 @@ public:
         double s_pares = 0.0f;
         int cont;
 
-        for(x= a + h, cont = 1; cont <=n;cont++){
+        for(x= a + h, cont = 1; cont <n;cont++){
             double y = f(x);
             if(cont%2==0){
                 s_pares +=y;
@@ -35,13 +37,30 @@ public:
     }
     double tres_octavos(int n){
 
-    }
+        double h = (b-a)/n;
+        double x = a;
+        double y = f(x);
+        double s1 = 0;
+        double s2 = 0;
 
+        for (int i = 1; i < n; ++i) {
+            x = x + h;
+            y = f(x);
+
+            if(i%3==0){
+                s1 = s1+y;
+            }else{
+                s2 = s2 +y;
+            }
+        }
+
+        double resultado = (3*h/8)*(f(a)+(3*s2)+(2*s1)+f(b));
+        return resultado;
+    }
 private:
     function<double(double)> f;
     double a;
     double b;
-    int n;
 };
 
 #endif //SIMPSONS_RULE_SIMPSON_H
